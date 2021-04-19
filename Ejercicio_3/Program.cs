1935106a5c38c5cdae0669e77840637167e7dd04
FELIPE IGNACIO MONTECINOS MUÑOZ <f.montecinos8@alumnos.santotomas.cl>
2021-04-18 22:32:18 -0400

    Se agrego ejercicio 1

---
+++ Ejercicio_3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Cree un programa de consola que solicite una distancia en kilómetros para ser recorrida en 6 horas, el script debe
+            //calcular a qué velocidad se desplazará el vehículo en recorrer la distancia ingresada por el cliente, debe mostrar el
+            //resultado por pantalla.
+
+            int Velocidad;
+            int Distancia;
+            int Tiempo = 6;
+
+            Console.WriteLine("Ingrese la distancia que desea recorrer: ");
+            Distancia = int.Parse(Console.ReadLine());
+
+            Velocidad = Distancia / Tiempo;
+
+            Console.WriteLine("Usted se desplazara a esta velocidad: "+Velocidad+(" Km"));
+
+            Console.ReadLine();
+        }
+        
+    }
+}
---
+++ Ejercicio_6/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese un numero: ");
+            int Num = int.Parse(Console.ReadLine());
+
+            for (int contador = 1; contador < Num; contador += 2)
+            {
+                Console.WriteLine(contador);
+            }
+            Console.WriteLine("Estos numeros son impares");
+            Console.ReadLine();
+        }
+    }
+}
+
+
+
